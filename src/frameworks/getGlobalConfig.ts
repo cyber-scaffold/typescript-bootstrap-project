512371ddd031a2153446843ae5032cdc6f35226a
frank.leo <1542874601@qq.com>
2023-10-09 10:14:28 +0800

    temp:临时节点标记 星期一 上午 10:14:27 十月 9日 2023 修改了 package.json 修改了 src/frameworks/getGlobalConfig.ts

--- src/frameworks/getGlobalConfig.ts
+++ src/frameworks/getGlobalConfig.ts
@@ -1,22 +1,37 @@
 import os from "os";
 import path from "path";
-import isDocker from "is-docker";
+import { merge } from "lodash";
+import pathExists from "path-exists";
 import { readFileSync } from "jsonfile";
 
+const defaultConfig = {
+  "server": {
+    "swagger": true,
+    "port": 28652
+  },
+  "mysql": {
+    "host": "0.0.0.0",
+    "port": 3306,
+    "username": "root",
+    "password": "gaea0571"
+  },
+  "redis": {
+    "host": "0.0.0.0",
+    "port": 6379
+  }
+};
+
 export function getGlobalConfig() {
-
-  let global_config_path;
-
-  if (isDocker()) {
-
-    global_config_path = path.join("/etc/", "/application/", "./runtime_config.json")
-
-  } else {
-
-    global_config_path = path.resolve(os.homedir(), "./runtime_config.json")
-
+  let global_etc_config = {};
+  let global_custmer_config = {};
+  const global_etc_config_path = path.join("/etc/", "/gaeaweb/", "./config.json");
+  if (pathExists.sync(global_etc_config_path)) {
+    global_etc_config = readFileSync(global_etc_config_path);
   };
-
-  const global_config = readFileSync(global_config_path);
-  return global_config;
+  const global_custmer_config_path = path.join(os.homedir(), "/.gaeaweb/", "./config.json");
+  if (pathExists.sync(global_custmer_config_path)) {
+    global_custmer_config = readFileSync(global_custmer_config_path);
+  };
+  const compose_config = merge(defaultConfig, global_etc_config, global_custmer_config);
+  return compose_config;
 };
