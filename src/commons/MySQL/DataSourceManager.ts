6bdcc3bbc56c7dd8414bb8a98180340a18844814
frankleo <1542874601@qq.com>
2025-07-01 15:27:32 +0800

    update

--- src/commons/MySQL/DataSourceManager.ts
+++ src/commons/MySQL/DataSourceManager.ts
@@ -3,6 +3,7 @@ import { injectable, inject } from "inversify";
 
 import { ApplicationConfigManager } from "@/commons/Application/ApplicationConfigManager";
 import { IOCContainer } from "@/commons/Application/IOCContainer";
+import { logger } from "@/utils/logger";
 
 /** 很多分库分表都是在应用层完成的,一般都是根据数据库名进行区分 **/
 @injectable()
@@ -16,7 +17,7 @@ export class DataSourceManager {
   ) { };
 
   /** 初始化 **/
-  public initialize() {
+  public async initialize() {
     const { mysql } = this.$ApplicationConfigManager.getRuntimeConfig();
     this.appDataSource = new DataSource({
       type: "mysql",
@@ -28,6 +29,8 @@ export class DataSourceManager {
       entities: [],
       synchronize: true
     });
+    await this.appDataSource.initialize();
+    logger.info("AppDataSource 连接成功!");
   };
 
   /** 根据数据库名称获取AppDataSource连接对象 **/
