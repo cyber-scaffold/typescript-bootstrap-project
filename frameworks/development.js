ee0ca0f78bc4f4c77883ddec4b13d2921720794a
frankleo <1542874601@qq.com>
2026-03-19 18:38:40 +0800

    update development script

--- frameworks/development.js
+++ frameworks/development.js
@@ -1,9 +1,12 @@
+const fs = require("fs");
 const path = require("path");
 const esbuild = require("esbuild");
 const nodemon = require("nodemon");
 const { green } = require("colors");
+const { promisify } = require("util");
 
 setImmediate(async () => {
+  await promisify(fs.rm)(path.resolve(process.cwd(), "./dist/"), { recursive: true, force: true });
   const context = await esbuild.context({
     entryPoints: [path.resolve(process.cwd(), "./src/index.ts")],
     bundle: true,
@@ -12,8 +15,10 @@ setImmediate(async () => {
     outdir: path.resolve(process.cwd(), "./dist/"),
     external: ["knex"]
   });
+  await context.rebuild();
   await context.watch();
   nodemon({
+    verbose: true,
     script: path.resolve(process.cwd(), "./dist/index.js"),
     ignore: ["**/frameworks/**"]
   }).on("start", () => {
@@ -22,5 +27,5 @@ setImmediate(async () => {
     console.log(green("nodemon quit!!!"));
   }).on("restart", (files) => {
     console.log(green("nodemon restart ... ..."));
-  })
+  });
 });
